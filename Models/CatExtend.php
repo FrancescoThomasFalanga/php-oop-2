b4f70407dd9aec2df39ba72d65e1872dcacbfee7
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-05-04 16:37:29 +0200

    add category in Extend

--- Models/CatExtend.php
+++ Models/CatExtend.php
@@ -2,11 +2,12 @@
 
 class CatExtend extends Product {
 
+    public $category = "cat";
     public $icon = "<i class='fa-solid fa-cat'></i>";
 
-    function __construct(string $type, string $imgString, string $title, string $price, string $categoryIcon, string $desc)
+    function __construct(string $type, string $imgString, string $title, string $price, string $desc)
     {
-        parent::__construct($type, $imgString, $title, $price, $categoryIcon, $desc);
+        parent::__construct($type, $imgString, $title, $price, $desc);
     }
 
 }
--- Models/DogExtend.php
+++ Models/DogExtend.php
@@ -2,11 +2,12 @@
 
 class DogExtend extends Product {
 
+    public $category = "dog";
     public $icon= "<i class='fa-solid fa-dog'></i>";
 
-    function __construct(string $type, string $imgString, string $title, string $price, string $categoryIcon, string $desc)
+    function __construct(string $type, string $imgString, string $title, string $price, string $desc)
     {
-        parent::__construct($type, $imgString, $title, $price, $categoryIcon, $desc);
+        parent::__construct($type, $imgString, $title, $price, $desc);
     }
 
 }
