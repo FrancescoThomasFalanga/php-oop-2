86850f64c7f878d748438c0c7985f4c77f6f52fc
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-05-05 17:20:05 +0200

    add weight

--- Models/db.php
+++ Models/db.php
@@ -9,17 +9,31 @@ $dogSnack = new Diet("Snack", "./img/dogs/biscotti_tacchino.webp", "Biscotti al 
 $dogSnack1 = new Diet("Snack", "./img/dogs/filetti_anatra.webp", "Bastoncini all'Anatra", "6.49", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "dog", "20/04/2023");
 $dogGrooming = new Product("Toelettatura", "./img/dogs/shampoo_bollicine.webp", "Shampoo Bollicine, mantello nero", "14.99", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "dog");
 
+$dogDiet->weight = 800;
+$dogAccessory->weight = 20;
+$dogAccessory1->weight = 12.5;
+$dogGame->weight = 6;
+$dogGame1->weight = 0.1;
+$dogSnack->weight = 250;
+$dogSnack1->weight = 200;
+
 $dogsProduct = [
     $dogDiet, $dogAccessory, $dogAccessory1, $dogGame, $dogGame1, $dogSnack, $dogSnack1, $dogGrooming
 ];
 
 
 $catDiet = new Diet("Alimentazione", "./img/cats/cibo_secco.webp", "Cibo Secco con Pesce", "20.99", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "cat", "10/12/2023");
-$catAccessory = new Diet("Alimentazione", "./img/cats/fontana_acqua.webp", "Fontana d'acqua per gatti", "19.99", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "cat", "21/07/2024");
+$catAccessory = new Accessory("Accessori", "./img/cats/fontana_acqua.webp", "Fontana d'acqua per gatti", "19.99", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "cat", "medie");
 $catAccessory1 = new Product("Graffiatoio", "./img/cats/tiragraffi.jpg", "Tiragraffi con pallina per gatto", "9.99", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "cat");
 $catGame = new Accessory("Gioco", "./img/cats/topino.jpg", "Topino Vibrante", "5.09", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "cat", "medie");
 $catGame1 = new Accessory("Gioco", "./img/cats/laser.webp", "Puntatore laser per Gatti", "3.59", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "cat", "piccole");
 $catLitter = new Accessory("Accessori", "./img/cats/lettiera_silicio.webp", "Lettiera Silicio per gatti", "5.29", "Lorem ipsum dolor sit amet consectetur, adipisicing elit.", "cat", "piccole");
+
+$catDiet->weight = 500;
+$catAccessory->weight = 4;
+$catGame->weight = 0.3;
+$catGame1->weight = 0.1;
+$catLitter->weight = 3;
 
 $catsProduct = [
     $catDiet, $catAccessory, $catAccessory1, $catGame, $catGame1, $catLitter
--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <?php
 
-//  common type of things: diet, accessory, game;
-//  different: for Dogs(snack, grooming) for Cats(litter);
-
 // All Product with their COMMON TYPE
 require_once "./Models/Product.php";
 
@@ -14,7 +11,6 @@ require_once "./Models/Accessory.php";
 
 // DB that Contains all the Product
 require_once "./Models/db.php";
-
 
 ?>
 
@@ -32,51 +28,135 @@ require_once "./Models/db.php";
 </head>
 <body>
     
+
+    <!-- COMMON CONTAINER -->
     <div class="container-fluid text-center">
 
+        <!-- E-COMMERCE TITLE -->
         <h1 class="pt-4">Shop Online Per Animali</h1>
+        <!-- /E-COMMERCE TITLE -->
         
 
-        <h2 class="border-top mt-4 pt-3 pb-3">Prodotti per Cani</h2>
+        <!-- DOGS PRODUCT -->
+        <h2 class="border-top mt-5 pt-3 pb-3">Prodotti per Cani</h2>
 
-        <div class="d-flex gap-4 flex-wrap">
+        <div class="d-flex gap-4 flex-wrap mb-5">
 
             <?php
                 foreach($dogsProduct as $singleProduct) {
                     ?>
+
+                    <!-- DOGS CARD -->
                     <div class="card" style="width: 18rem;">
+
+                        <!-- CARD IMG -->
                         <img src="<?php echo $singleProduct->imgString ?>" class="" alt="..." style="height:200px; object-fit:contain">
+                        <!-- CARD IMG -->
+
+
+                        <!-- CARD TITLE + DESCRIPTION -->
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $singleProduct->title ?></h5>
                             <p class="card-text"><?php echo $singleProduct->desc ?></p>
                         </div>
+                        <!-- CARD TITLE + DESCRIPTION -->
+
+
+                        <!-- CARD SUB-CONTENT -->
                         <ul class="list-group list-group-flush">
+
+                            <!-- PRODUCT CATEGORY -->
                             <li class="list-group-item">Categoria: <strong><?php echo $singleProduct->type ?></strong></li>
+                            <!-- /PRODUCT CATEGORY -->
+
                             <?php
                                 if($singleProduct->type == "Alimentazione" || $singleProduct->type == "Snack") {
                                     ?>
+
+                                    <!-- EXPIRATION DATA -->
                                     <li class="list-group-item">Data Di Scadenza: <strong><?php echo $singleProduct->expiration_data ?></strong></li>
+                                    <!-- /EXPIRATION DATA -->
+
+
+                                    <!-- DIET + SNACK QUANTITY -->
+                                    <li class="list-group-item">
+                                        Quantità: 
+                                        <strong>
+                                            <?php 
+                                                try {
+
+                                                    echo $singleProduct->getWeight($singleProduct->weight) . " g";
+
+                                                } catch(Exception $e) {
+
+                                                    echo $e->getMessage();
+
+                                                }
+                                            ?> 
+                                        </strong>
+                                    </li>
+                                    <!-- /DIET + SNACK QUANTITY -->
+
                                     <?php
+
                                 } else if($singleProduct->type == "Accessori" || $singleProduct->type == "Gioco") {
                                     ?>
+
+                                    <!-- ACCESSORY + GAME SIZE -->
                                     <li class="list-group-item">Dimensioni: <strong><?php echo $singleProduct->size ?></strong></li>
+                                    <!-- /ACCESSORY + GAME SIZE -->
+
+
+                                    <!-- ACCESSORY + GAME WEIGHT -->
+                                    <li class="list-group-item">
+                                        Peso: 
+                                        <strong>
+                                            <?php 
+                                                try {
+
+                                                    echo $singleProduct->getWeight($singleProduct->weight) . " kg";
+
+                                                } catch(Exception $e) {
+
+                                                    echo $e->getMessage();
+
+                                                }
+                                            ?> 
+                                        </strong>
+                                    </li>
+                                    <!-- /ACCESSORY + GAME WEIGHT -->
+
                                     <?php
                                 }
                             ?>
+
+                            <!-- PRODUCT PRIZE -->
                             <li class="list-group-item">Prezzo: <strong><?php echo $singleProduct->price ?>€</strong></li>
+                            <!-- /PRODUCT PRIZE -->
+
                         </ul>
+                        <!-- /CARD SUB-CONTENT -->
+
+
+                        <!-- CATEGORY FOR THE ICON -->
                         <div class="card-body">
                             Categoria: <a href="#" class="card-link"><?php echo $singleProduct->getIcon() ?></a>
                         </div>
+                        <!-- CATEGORY FOR THE ICON -->
+
                     </div>
+                    <!-- /DOGS CARD -->
+
                     <?php
                 }
             ?>
 
         </div>
+        <!-- /DOGS PRODUCT -->
 
 
 
+        <!-- CATS PRODUCT -->
         <h2 class="border-top mt-5 pt-3 pb-3">Prodotti per Gatti</h2>
 
         <div class="d-flex gap-4 flex-wrap mb-5">
@@ -84,39 +164,117 @@ require_once "./Models/db.php";
             <?php
                 foreach($catsProduct as $singleProduct) {
                     ?>
+
+                    <!-- CATS CARD -->
                     <div class="card" style="width: 18rem;">
-                    <img src="<?php echo $singleProduct->imgString ?>" class="" alt="..." style="height:200px; object-fit:contain">
+
+                        <!-- CARD IMG -->
+                        <img src="<?php echo $singleProduct->imgString ?>" class="" alt="..." style="height:200px; object-fit:contain">
+                        <!-- CARD IMG -->
+
+
+                        <!-- CARD TITLE + DESCRIPTION -->
                         <div class="card-body">
                             <h5 class="card-title"><?php echo $singleProduct->title ?></h5>
                             <p class="card-text"><?php echo $singleProduct->desc ?></p>
                         </div>
+                        <!-- CARD TITLE + DESCRIPTION -->
+
+
+                        <!-- CARD SUB-CONTENT -->
                         <ul class="list-group list-group-flush">
+
+                            <!-- PRODUCT CATEGORY -->
                             <li class="list-group-item">Categoria: <strong><?php echo $singleProduct->type ?></strong></li>
+                            <!-- /PRODUCT CATEGORY -->
+
                             <?php
                                 if($singleProduct->type == "Alimentazione" || $singleProduct->type == "Snack") {
                                     ?>
+
+                                    <!-- EXPIRATION DATA -->
                                     <li class="list-group-item">Data Di Scadenza: <strong><?php echo $singleProduct->expiration_data ?></strong></li>
+                                    <!-- /EXPIRATION DATA -->
+
+
+                                    <!-- DIET + SNACK QUANTITY -->
+                                    <li class="list-group-item">
+                                        Quantità: 
+                                        <strong>
+                                            <?php 
+                                                try {
+
+                                                    echo $singleProduct->getWeight($singleProduct->weight) . " g";
+
+                                                } catch(Exception $e) {
+
+                                                    echo $e->getMessage();
+
+                                                }
+                                            ?> 
+                                        </strong>
+                                    </li>
+                                    <!-- /DIET + SNACK QUANTITY -->
+
                                     <?php
+
                                 } else if($singleProduct->type == "Accessori" || $singleProduct->type == "Gioco") {
                                     ?>
+
+                                    <!-- ACCESSORY + GAME SIZE -->
                                     <li class="list-group-item">Dimensioni: <strong><?php echo $singleProduct->size ?></strong></li>
+                                    <!-- /ACCESSORY + GAME SIZE -->
+
+
+                                    <!-- ACCESSORY + GAME WEIGHT -->
+                                    <li class="list-group-item">
+                                        Peso: 
+                                        <strong>
+                                            <?php 
+                                                try {
+
+                                                    echo $singleProduct->getWeight($singleProduct->weight) . " kg";
+
+                                                } catch(Exception $e) {
+
+                                                    echo $e->getMessage();
+
+                                                }
+                                            ?> 
+                                        </strong>
+                                    </li>
+                                    <!-- /ACCESSORY + GAME WEIGHT -->
+
                                     <?php
                                 }
                             ?>
+
+                            <!-- PRODUCT PRIZE -->
                             <li class="list-group-item">Prezzo: <strong><?php echo $singleProduct->price ?>€</strong></li>
+                            <!-- /PRODUCT PRIZE -->
+
                         </ul>
+                        <!-- /CARD SUB-CONTENT -->
+
+
+                        <!-- CATEGORY FOR THE ICON -->
                         <div class="card-body">
                             Categoria: <a href="#" class="card-link"><?php echo $singleProduct->getIcon() ?></a>
                         </div>
+                        <!-- CATEGORY FOR THE ICON -->
+
                     </div>
+                    <!-- /CATS CARD -->
+
                     <?php
                 }
             ?>
 
         </div>
-
+        <!-- /CATS PRODUCT -->
 
     </div>
+    <!-- COMMON CONTAINER -->
 
 </body>
 </html>
